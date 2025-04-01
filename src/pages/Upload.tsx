ead599c6cb7eb12b8f2a0dea5722b1fcef4d809f
Royal <royal.21ai040@sode-edu.in>
2025-04-01 21:29:35 +0530

    add upload code

---
+++ src/pages/Upload.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import FileUpload from '@/components/upload/FileUpload';
+
+const Upload = () => {
+  return (
+    <div className="min-h-[calc(100vh-180px)] flex items-center justify-center">
+      <FileUpload />
+    </div>
+  );
+};
+
+export default Upload;
